e064521f701762d07ed345d90c542feb8a09df5c
Chanho Won <wonch30@gmail.com>
2023-04-24 21:20:30 +0900

    feature: AlcCard - [x] frame 완성

--- src/atoms/AlcCard/AlcCard.jsx
+++ src/atoms/AlcCard/AlcCard.jsx
@@ -6,11 +6,11 @@ export default function AlcCard() {
   return (
     <div className={styles.frame}>
       <div className={styles.imgArea}>
-        <img src={sample} alt="" />
+        <img className={styles.myImg} src={sample} alt="" />
       </div>
       <div className={styles.textArea}>
-        <p>asd</p>
-        <p>asd</p>
+        <p className={styles.name}>루시안 블랙</p>
+        <p className={styles.tag}>#달콤 # 쌀쌀</p>
       </div>
     </div>
   );
